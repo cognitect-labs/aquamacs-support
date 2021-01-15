1e63f85bae6ab37216c9778496db4bc93514a92b
Robert Randolph <audiolabs@gmail.com>
2021-01-15 12:02:23 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# What is this
+
+A quick-start [Clojure](https://clojure.org) development environment for use with [Aquamacs](https://aquamacs.org).
+
+# Setup for experienced developers
+
+This setup does not reflect final clojure.org quick-start instructions. It assumes familiarity with git and the command line.
+
+* Install [Aquamacs](https://aquamacs.org/download.html) 3.5 or newer.
+* Launch, then quit, Aquamacs.app to create the preferences directory **OR**
+  * `mkdir ~/Library/Preferences/Aquamacs\ Emacs/` in your terminal
+* Clone this repository in `~/Library/Preferences/Aquamacs Emacs/`
+* Launch Aquamacs.app
+
+# Setup for new Clojure enthusiasts
+
+These instructions assume a downloadable `Preferences.el` on clojure.org, and that the user has Clojure.
+
+* Install [Aquamacs](https://aquamacs.org/download.html) 3.5 or newer.
+* Launch, then quit, Aquamacs.app to create the preferences directory.
+* Download the setup package (link here)
+* Copy `Preferences.el` to `~/Library/Preferences/Aquamacs Emacs/`
+* Launch Aquamacs.app
+
+# Using Clojure/Aquamacs
+
+To begin with Clojure:
+
+* Launch Aquamacs.app
+* Create a new file with the extension `.clj`
+* Press Control-c then Control-z
+* Type your preferred clojure startup command, the default of "clojure" is fine to start.
+* Your window will split, and a Clojure REPL will appear with `user=>`
+
+# Features
+
+* [Clojure Mode](https://github.com/clojure-emacs/clojure-mode)
+* Paredit
+
+# Hotkeys
+
+TODO: Replace emacs function name with simple description
+
+## Paredit
+
+Hotkey | Function
+| --- | --- |
+| M-[ 	| paredit-wrap-square |
+| M-{ 	| paredit-wrap-curly |
+| M-<right> 	| forward-sexp |
+| M-<left> 	| backward-sexp |
+| M-<up> 	| backward-up-list |
+| M-<down> 	| down-list |
+| <A-return> 	| paredit-newline |
+
+## Clojure
+     
+Hotkey | Function
+| --- | --- |
+| C-: | clojure-toggle-keyword-string |
+| C-c <space> | clojure-align |
+| C-M-x 	| lisp-eval-defun |
+| C-x C-e | lisp-eval-last-sexp |
+| C-c C-e | lisp-eval-last-sexp |
+| C-c C-z | run-clojure |
+| C-c C-r | lisp-eval-region |
+| C-x C-e | lisp-eval-last-sexp |
+| C-c C-a | lisp-show-arglist |
+| C-c C-c | lisp-compile-defun |
+| C-c C-d | lisp-describe-sym |
+| C-c C-e | lisp-eval-defun |
+| C-c C-f | lisp-show-function-documentation |
+| C-c C-k | lisp-compile-file |
+| C-c C-l | lisp-load-file |
+| C-c C-n | lisp-eval-form-and-next |
+| C-c C-p | lisp-eval-paragraph |
+| C-c C-r | lisp-eval-region |
+| C-c C-v | lisp-show-variable-documentation |
+| C-c C-z | run-clojure |
+| C-M-x 	| lisp-eval-defun |
+| C-M-q 	| indent-sexp |
---
+++ preferences.el
@@ -0,0 +1,92 @@
+;; These options are insecure, but currently necessary for package download in Aquamacs.
+;; Disable package signature checking and use TLS 1.2 or lower.
+;; Use at your own risk.
+(setq package-check-signature nil)
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+(require 'package)
+;;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+;;(package-refresh-contents)
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  (add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
+
+(use-package paredit
+  :ensure t)
+
+(use-package clojure-mode
+  :ensure t)
+
+(use-package inf-lisp
+  :ensure t)
+
+(use-package exec-path-from-shell
+  :ensure t)
+
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+(setq inferior-lisp-program "clojure")
+
+;; TODO Add command history
+(defun run-clojure (cmd)
+  (interactive (list (read-from-minibuffer "Command:" "clojure")))
+  (let ((default-directory (if (boundp 'clojure-project-root-path)
+			                   (clojure-project-root-path)
+			                 default-directory))
+	    cb (curent-buffer))
+    (run-lisp cmd)
+    (switch-to-buffer cb)
+    (switch-to-buffer-other-window "*inferior-lisp*")))
+
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code." t)
+(add-hook 'clojure-mode-hook          (lambda () (paredit-mode +1)))
+(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
+(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+
+(eval-after-load 'paredit
+  '(progn
+     (define-key paredit-mode-map (kbd "M-[") 'paredit-wrap-square)
+     (define-key paredit-mode-map (kbd "M-{") 'paredit-wrap-curly)
+     (define-key paredit-mode-map (kbd "M-<right>") 'forward-sexp)
+     (define-key paredit-mode-map (kbd "M-<left>") 'backward-sexp)
+     (define-key paredit-mode-map (kbd "M-<up>") 'backward-up-list)
+     (define-key paredit-mode-map (kbd "M-<down>") 'down-list)
+     (define-key paredit-mode-map (kbd "<A-return>") 'paredit-newline)))
+
+(defun add-clojure-eval ()
+  (define-key clojure-mode-map (kbd "C-M-x") 'lisp-eval-defun)
+  (define-key clojure-mode-map (kbd "C-x C-e") 'lisp-eval-last-sexp)
+  (define-key clojure-mode-map (kbd "C-c C-e") 'lisp-eval-last-sexp)
+  (define-key clojure-mode-map (kbd "C-c C-z") 'run-clojure)
+  (define-key clojure-mode-map (kbd "C-c C-r") 'lisp-eval-region)
+  (define-key clojure-mode-map (kbd "C-x C-e") 'lisp-eval-last-sexp)
+  (define-key clojure-mode-map (kbd "C-c C-a") 'lisp-show-arglist)
+  (define-key clojure-mode-map (kbd "C-c C-c") 'lisp-compile-defun)
+  (define-key clojure-mode-map (kbd "C-c C-d") 'lisp-describe-sym)
+  (define-key clojure-mode-map (kbd "C-c C-e") 'lisp-eval-defun)
+  (define-key clojure-mode-map (kbd "C-c C-f") 'lisp-show-function-documentation)
+  (define-key clojure-mode-map (kbd "C-c C-k") 'lisp-compile-file)
+  (define-key clojure-mode-map (kbd "C-c C-l") 'lisp-load-file)
+  (define-key clojure-mode-map (kbd "C-c C-n") 'lisp-eval-form-and-next)
+  (define-key clojure-mode-map (kbd "C-c C-p") 'lisp-eval-paragraph)
+  (define-key clojure-mode-map (kbd "C-c C-r") 'lisp-eval-region)
+  (define-key clojure-mode-map (kbd "C-c C-v") 'lisp-show-variable-documentation)
+  (define-key clojure-mode-map (kbd "C-c C-z") 'run-clojure)
+  (define-key clojure-mode-map (kbd "C-M-x") 'lisp-eval-defun)
+  (define-key clojure-mode-map (kbd "C-M-q") 'indent-sexp))
+(add-hook 'clojure-mode-hook 'add-clojure-eval)
+
+
+
