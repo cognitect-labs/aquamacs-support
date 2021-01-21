76646f9d5fa4671d3e048a7fcd60bec97e6612ec
Robert Randolph <audiolabs@gmail.com>
2021-01-21 09:17:09 -0500

    map clojure-command-proprely

--- preferences.el
+++ preferences.el
@@ -41,7 +41,7 @@
 
 (eval-after-load 'clojure-mode
   '(progn
-     (define-key paredit-mode-map (kbd "C-c C-z") 'run-clojure-cmd)
+     (define-key paredit-mode-map (kbd "C-c C-z") 'run-clojure-command)
      (define-key paredit-mode-map (kbd "C-z") '(lambda () (run-lisp "clojure")))
      (define-key paredit-mode-map (kbd "C-M-x") 'lisp-eval-defun) ;; primary eval mode
      (define-key paredit-mode-map (kbd "C-c C-e") 'lisp-eval-defun)
